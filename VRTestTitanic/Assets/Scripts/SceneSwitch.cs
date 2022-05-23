4765b82f769da4b7adc4535c132a4b36d7d9a585
Harper Bennett <harper.bennett@mymail.champlain.edu>
2022-05-23 09:56:17 -0400

    Made the prefab for switching screens- the player "grabs" the cube and it switches to a different scene

---
+++ VRTestTitanic/Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,132 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Valve.VR.InteractionSystem;
+
+public class SceneSwitch : MonoBehaviour
+{
+	private Vector3 oldPosition;
+	private Quaternion oldRotation;
+
+	private float attachTime;
+
+	private Hand.AttachmentFlags attachmentFlags = Hand.defaultAttachmentFlags & (~Hand.AttachmentFlags.SnapOnAttach) & (~Hand.AttachmentFlags.DetachOthers) & (~Hand.AttachmentFlags.VelocityMovement);
+
+	private Interactable interactable;
+
+	public string sceneName;
+
+	//-------------------------------------------------
+	void Awake()
+	{
+		interactable = this.GetComponent<Interactable>();
+	}
+
+
+	//-------------------------------------------------
+	// Called when a Hand starts hovering over this object
+	//-------------------------------------------------
+	private void OnHandHoverBegin(Hand hand)
+	{
+		hand.ShowGrabHint();
+	}
+
+
+	//-------------------------------------------------
+	// Called when a Hand stops hovering over this object
+	//-------------------------------------------------
+	private void OnHandHoverEnd(Hand hand)
+	{
+		hand.HideGrabHint();
+	}
+
+
+	//-------------------------------------------------
+	// Called every Update() while a Hand is hovering over this object
+	//-------------------------------------------------
+	private void HandHoverUpdate(Hand hand)
+	{
+		GrabTypes startingGrabType = hand.GetGrabStarting();
+		bool isGrabEnding = hand.IsGrabEnding(this.gameObject);
+
+		if (interactable.attachedToHand == null && startingGrabType != GrabTypes.None)
+		{
+			// Save our position/rotation so that we can restore it when we detach
+			//oldPosition = transform.position;
+			//oldRotation = transform.rotation;
+
+			// Call this to continue receiving HandHoverUpdate messages,
+			// and prevent the hand from hovering over anything else
+			hand.HoverLock(interactable);
+
+			// Attach this object to the hand
+			SceneManager.LoadScene(sceneName);
+			hand.AttachObject(gameObject, startingGrabType, attachmentFlags);
+		}
+		else if (isGrabEnding)
+		{
+			// Detach this object from the hand
+			hand.DetachObject(gameObject);
+
+			// Call this to undo HoverLock
+			hand.HoverUnlock(interactable);
+
+			// Restore position/rotation
+			//transform.position = oldPosition;
+			//transform.rotation = oldRotation;
+		}
+	}
+
+
+	//-------------------------------------------------
+	// Called when this GameObject becomes attached to the hand
+	//-------------------------------------------------
+	private void OnAttachedToHand(Hand hand)
+	{
+
+	}
+
+
+
+	//-------------------------------------------------
+	// Called when this GameObject is detached from the hand
+	//-------------------------------------------------
+	private void OnDetachedFromHand(Hand hand)
+	{
+	}
+
+
+	//-------------------------------------------------
+	// Called every Update() while this GameObject is attached to the hand
+	//-------------------------------------------------
+	private void HandAttachedUpdate(Hand hand)
+	{
+	}
+
+	private bool lastHovering = false;
+	private void Update()
+	{
+		//if (interactable.isHovering != lastHovering) //save on the .tostrings a bit
+		//{
+		//	lastHovering = interactable.isHovering;
+		//}
+	}
+
+
+	//-------------------------------------------------
+	// Called when this attached GameObject becomes the primary attached object
+	//-------------------------------------------------
+	//private void OnHandFocusAcquired(Hand hand)
+	//{
+	//}
+
+
+	//-------------------------------------------------
+	// Called when another attached GameObject becomes the primary attached object
+	//-------------------------------------------------
+	//private void OnHandFocusLost(Hand hand)
+	//{
+	//}
+}
