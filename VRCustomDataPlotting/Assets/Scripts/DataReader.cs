eaf388213d8ee63723f5e9ce8d382f5dd0a38fd2
Lucia Hughes <lucia.hughes@mymail.champlain.edu>
2022-05-24 13:16:37 -0400

    fixing ignore

--- VRCustomDataPlotting/Assets/Scripts/DataReader.cs
+++ VRCustomDataPlotting/Assets/Scripts/DataReader.cs
@@ -33,6 +33,7 @@ public class DataReader : MonoBehaviour
             {
                 string[] fields = csvParser.ReadFields();
                 
+
             }
             //read data
             while (!csvParser.EndOfData)
