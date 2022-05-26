2ac8845658a3712b5e4fa8d3eea845322de8cc67
Evan Koppers <evan.koppers@mymail.champlain.edu>
2022-05-26 17:17:30 -0400

    switching over the shader type i also think i broke unity when i deleted the library, so, thats fun

--- 5-24-22 EVAN/Assets/Materials/HeatMapCode.shader
+++ 5-24-22 EVAN/Assets/Materials/HeatMapCode.shader
@@ -35,6 +35,21 @@ Shader "Unlit/HeatMapCode"
             sampler2D _MainTex;
             float4 _MainTex_ST;
 
+            float4 _Color0;
+            float4 _Color1;
+            float4 _Color2;
+            float4 _Color3;
+            float4 _Color4;
+
+
+            float _Range0;
+            float _Range1;
+            float _Range2;
+            float _Range3;
+            float _Range4;
+            float _Diameter;
+            float _Strength;
+
             v2f vert (appdata v)
             {
                 v2f o;
@@ -44,13 +59,87 @@ Shader "Unlit/HeatMapCode"
                 return o;
             }
 
+            float4 colors[5];
+            float pointranges[5];
+
+            float _Hits[3 * 32];
+            float _HitCount = 0;
+
+            void initalize()
+            {
+                colors[0] = float4(0,0,0,0);
+                colors[1] = float4(1, 0, 0, 1);
+                colors[2] = float4(0, 1, 0, 1);
+                colors[3] = float4(0, 0, 1, 1);
+                colors[4] = float4(1, 1, 1, 1);
+                pointranges[0] = 0;
+                pointranges[1] = 0.25;
+                pointranges[2] = 0.5;
+                pointranges[3] = 0.75;
+                pointranges[4] = 1.0;
+
+                _HitCount = 1;
+                _Hits[0] = 0;
+                _Hits[1] = 1;
+                _Hits[2] = 2;
+            }
+
+            float distsq(float2 a, float2 b)
+            {
+                float area_of_effect_size = 1.0f;
+
+                return  pow(max(0.0, 1.0 - distance(a, b) / area_of_effect_size), 2.0);
+            }
+
+            float4 pixelHeat(float weight)
+            {
+                if (weight <= pointranges[0])
+                {
+                    return colors[0];
+                }
+                
+                if (weight >= pointranges[4])
+                {
+                    return colors[4];
+                }
+
+                for (int i = 1; i < 5; i++)
+                {
+                    if (weight < pointranges[i])
+                    {
+                        float dist_from_lower_point = weight - pointranges[i - 1];
+                        float size_of_point_range = pointranges[i] - pointranges[i - 1];
+                        float ratio = dist_from_lower_point / size_of_point_range;
+                        float4 color_range = colors[i] - colors[i - 1];
+                        float4 color_contribution = color_range * ratio;
+
+                        return colors[i - 1] + color_contribution;
+                    }
+                }
+
+                return colors[0];
+            }
+
             fixed4 frag (v2f i) : SV_Target
             {
-                // sample the texture
                 fixed4 col = tex2D(_MainTex, i.uv);
-                // apply fog
-                UNITY_APPLY_FOG(i.fogCoord, col);
-                return col;
+
+            float2 uv = i.uv;
+            uv = uv * 4 - float2(2.0, 2.0);
+
+            float totalWeight = 0;
+            for (int i = 0; i < _HitCount; i++)
+            {
+                float pt_intensity = _Hits[i * 3 + 2];
+                float2 work_pt = float2(_Hits[i * 3], _Hits[i*3 +1]) * pt_intensity;
+
+                totalWeight += 0.5 * distsq(uv, work_pt) * pt_intensity;
+
+            }
+
+            float4 heat = pixelHeat(totalWeight);
+
+            return col + heat;
             }
             ENDCG
         }
--- 5-24-22 EVAN/Assets/Materials/HeatMapShaderMath.cs
+++ 5-24-22 EVAN/Assets/Materials/HeatMapShaderMath.cs
@@ -13,121 +13,12 @@ public class HeatMapShaderMath : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        texture = new Texture2D(Screen.width, Screen.height);
-        textureFloats = new Color[Screen.width,Screen.height];
 
-        for (int y = 0; y < texture.height; y += 1)
-        {
-            for (int x = 0; x < texture.width; x += 1)
-            {
-                texture.SetPixel(x, y, new Color(0,0,0,0));
-                textureFloats[x,y]= new Color(0,0,0,0);
-            }
-        }
-
-        for (int y = 0; y < texture.height; y+=10)
-        {
-            for (int x = 0; x < texture.width; x+=10)
-            {
-                Ray ray = Camera.main.ScreenPointToRay(new Vector2(x, y));
-                RaycastHit[] hits = Physics.RaycastAll(ray, 100f);
-
-                if(hits.Length > 0)
-                {
-                    Color color = new Color(hits.Length/5f, 0, 0, 1);
-                    for (int i = x - 5; i <= x + 5; i++)
-                    {
-                        for (int j = y - 5; j <= y + 5; j++)
-                        {
-                            //Color temp = color - new Color(((0.2f) * new Vector2(Mathf.Abs(i - x), Mathf.Abs(j - y)).magnitude), 0, 0, 0);
-                            //textureFloats[i, j] += temp;
-                            texture.SetPixel(i, j, color);
-                        }
-                    }
-                    /*
-                    for (int i = x - 10; i <= x + 10; i++)
-                    {
-                        for(int j = y - 10; j <= y + 10; j++)
-                        {
-                            Color temp = color - new Color(((0.2f) * new Vector2(Mathf.Abs(i-x), Mathf.Abs(j-y)).magnitude), 0, 0, 0);
-                            //textureFloats[i, j] += temp;
-                            texture.SetPixel(i,j, temp + texture.GetPixel(i,j));
-                        }
-                    }
-                    */
-                }
-
-                //Debug.Log(x + " " + y + " " + hits.Length);
-                //Debug.Log(color);
-            }
-        }
-
-       /*
-        for (int y = 0; y < texture.height; y ++)
-        {
-            for (int x = 0; x < texture.width; x ++)
-            {
-                texture.SetPixel(x,y, textureFloats[x, y]);
-                Debug.Log(textureFloats[x, y]);
-            }
-        }
-       */
-        
-
-        texture.Apply();
-
-        material.SetTexture("_Texture", texture);
     }
 
     // Update is called once per frame
     void Update()
     {
-        for (int y = 0; y < texture.height; y += 1)
-        {
-            for (int x = 0; x < texture.width; x += 1)
-            {
-                texture.SetPixel(x, y, new Color(0, 0, 0, 0));
-                textureFloats[x, y] = new Color(0, 0, 0, 0);
-            }
-        }
-
-        for (int y = 0; y < texture.height; y += 10)
-        {
-            for (int x = 0; x < texture.width; x += 10)
-            {
-                Ray ray = Camera.main.ScreenPointToRay(new Vector2(x, y));
-                RaycastHit[] hits = Physics.RaycastAll(ray, 100f);
-
-
-                if (hits.Length > 0)
-                {
-                    Color color = new Color(hits.Length / 5f, 0, 0, 1);
-                    for (int i = x - 5; i <= x + 5; i++)
-                    {
-                        for (int j = y - 5; j <= y + 5; j++)
-                        {
-                            //Color temp = color - new Color(((0.2f) * new Vector2(Mathf.Abs(i - x), Mathf.Abs(j - y)).magnitude), 0, 0, 0);
-                            //textureFloats[i, j] += temp;
-                            texture.SetPixel(i, j, color);
-                        }
-                    }
-                    /*
-                    for (int i = x - 10; i <= x + 10; i++)
-                    {
-                        for(int j = y - 10; j <= y + 10; j++)
-                        {
-                            Color temp = color - new Color(((0.2f) * new Vector2(Mathf.Abs(i-x), Mathf.Abs(j-y)).magnitude), 0, 0, 0);
-                            //textureFloats[i, j] += temp;
-                            texture.SetPixel(i,j, temp + texture.GetPixel(i,j));
-                        }
-                    }
-                    */
-                }
-
-                //Debug.Log(x + " " + y + " " + hits.Length);
-                //Debug.Log(color);
-            }
-        }
-        texture.Apply();
+        
     }
 }
