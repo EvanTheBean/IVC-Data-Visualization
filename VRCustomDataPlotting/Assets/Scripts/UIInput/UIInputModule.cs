c35b0a30321c0ad09422265a3e0d8f9498461688
Lucia Hughes <lucia.hughes@mymail.champlain.edu>
2022-06-06 12:24:18 -0400

    Trying out some input stuff

---
+++ VRCustomDataPlotting/Assets/Scripts/UIInput/UIInputModule.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+//public class UIInputModule : PointerInputModule
+//{
+//
+//    public override void Process()
+//    {
+//        if (GetPointerData(id, out EventSystems.PointerEventData data);
+//        EventSystem.current.RaycastAll(m_PointerData, m_RaycastResultCache);
+//    }
+//}
--- VRCustomDataPlotting/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ VRCustomDataPlotting/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -105,6 +105,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Boolean Test
+        {
+            get
+            {
+                return SteamVR_Actions.default_Test;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
