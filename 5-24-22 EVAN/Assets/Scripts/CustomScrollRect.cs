e148cd3693e9707eaae04ae52c1b3941986b0387
Evan Koppers <evan.koppers@mymail.champlain.edu>
2022-06-13 14:30:37 -0400

    start custom scroll

--- 5-24-22 EVAN/Assets/Scripts/CustomScrollRect.cs
+++ 5-24-22 EVAN/Assets/Scripts/CustomScrollRect.cs
@@ -10,9 +10,13 @@ public class CustomScrollRect : MonoBehaviour, IBeginDragHandler, IDragHandler, 
     public Image area;
     public GameObject viewPort, contentBox;
 
+    public Vector3 dragStart, contentStart;
+
+    public float size;
     // Start is called before the first frame update
     void Start()
     {
+        size = contentBox.transform.GetChild(0).gameObject.GetComponent<RectTransform>().rect.width;
     }
 
     // Update is called once per frame
@@ -24,6 +28,8 @@ public class CustomScrollRect : MonoBehaviour, IBeginDragHandler, IDragHandler, 
     public void OnBeginDrag(PointerEventData data)
     {
         Debug.Log("Begin Drag");
+        dragStart = data.position;
+        contentStart = contentBox.transform.position;
     }
 
     public void OnEndDrag(PointerEventData data)
@@ -34,6 +40,7 @@ public class CustomScrollRect : MonoBehaviour, IBeginDragHandler, IDragHandler, 
     public void OnDrag(PointerEventData data)
     {
         Debug.Log("Dragging");
+        contentBox.transform.position += new Vector3(contentBox.transform.position.x + Mathf.Sign(data.position.x - dragStart.x), contentBox.transform.position.y, contentBox.transform.position.z);
     }
 
 
