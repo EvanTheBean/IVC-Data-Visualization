82d1142132e221df04b2eb27c0a67713558694bd
Evan Koppers <evan.koppers@mymail.champlain.edu>
2022-06-13 13:43:44 -0400

    make custom scroll view scritp first test of on drag

---
+++ 5-24-22 EVAN/Assets/Scripts/CustomScrollRect.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using Valve.VR;
+
+public class CustomScrollRect : MonoBehaviour
+{
+    public Image area;
+    public GameObject viewPort, contentBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnBeginDrag(PointerEventData data)
+    {
+        Debug.Log("BeginDrag");
+    }
+
+    void OnEndDrag(PointerEventData data)
+    {
+        Debug.Log("End Drag");
+    }
+
+
+}
