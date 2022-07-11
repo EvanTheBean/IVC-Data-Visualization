a018abb1f8b7e95bf9f04e012a17a610ee9724e7
Lucia Hughes <luxiextra@gmail.com>
2022-07-11 14:14:51 -0400

    Adds required Networking packages
    * Links project to EMC Unity Services account
    * Adds networking scripts for lobby creation from earlier prototypes

---
+++ VizBoXRDesktop/Assets/Scripts/LogInAuthenticator.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using System;
+
+// ===== Log In Authenticator =====
+//  - Manages the log in process
+//  - Automatically logs in the user anonymously
+
+public class LogInAuthenticator : MonoBehaviour
+{
+    private void Start()
+    {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        SignIn();    
+    }
+
+    public async void SignIn() //Signs in the player with a new anonymous id
+    {
+        await UnityServices.InitializeAsync();
+
+        //Add listeners
+        AuthenticationService.Instance.SignedIn += OnSignIn;
+        AuthenticationService.Instance.SignedOut += OnSignOut;
+
+        try //Anonymous Sign In
+        {
+            if (!AuthenticationService.Instance.IsSignedIn)
+                await AuthenticationService.Instance.SignInAnonymouslyAsync(); // Don't sign out later, since that changes the anonymous token, which would prevent the player from exiting lobbies they're already in.
+        }
+        catch
+        {
+            UnityEngine.Debug.LogError("Failed to login. Did you remember to set your Project ID under Services > General Settings?");
+            Debug.LogError("LIAuth: Failed to login.");
+            throw;
+        }
+    }
+
+    private void OnSignIn()
+    {
+        Debug.Log("LIAuth: Signed in successfully.");
+    }
+
+    private void OnSignOut()
+    {
+        Debug.Log("LIAuth: Signed out successfully.");
+    }
+}
---
+++ VizBoXRMobile/Assets/Scripts/RelayLobbyManager.cs
@@ -0,0 +1,136 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+
+public class RelayLobbyManager : MonoBehaviour
+{
+    const int MAX_CONNECTIONS = 20; // Maximum amount of connections that the lobby can handle. CANNOT EXCEED 100
+
+    public string tryJoinCode { set; get; } // Join code inputted by user
+    string lobbyJoinCode = ""; // Join code obtained from lobby
+
+
+    public void CreateLobby() 
+    {
+        StartCoroutine(ConfigureTransportAndStartNgoAsHost()); //Set up networking        
+    }
+
+    public void Join()
+    {
+        StartCoroutine(ConfigureTransportAndStartNgoAsConnectingPlayer());
+    }
+
+    public string GetJoinCode()
+    {
+        return lobbyJoinCode;
+    }
+
+    IEnumerator ConfigureTransportAndStartNgoAsHost() // Create Lobby
+    {
+        var serverRelayUtilityTask = AllocateRelayServerAndGetJoinCode(MAX_CONNECTIONS);
+        while (!serverRelayUtilityTask.IsCompleted)
+        {
+            yield return null;
+        }
+        if (serverRelayUtilityTask.IsFaulted)
+        {
+            Debug.Log("RLManager: Exception thrown when attempting to start Relay Server. Server not started. Exception: " + serverRelayUtilityTask.Exception.Message);
+            yield break;
+        }
+
+        var (ipv4address, port, allocationIdBytes, connectionData, key, joinCode) = serverRelayUtilityTask.Result;
+
+        lobbyJoinCode = joinCode;
+
+        // Display the join code to the user.
+        Debug.Log("RLManager: Server allocated.");
+        Debug.Log("Join Code = " + joinCode);
+
+        // The .GetComponent method returns a UTP NetworkDriver (or a proxy to it)
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetHostRelayData(ipv4address, port, allocationIdBytes, key, connectionData, true);
+        NetworkManager.Singleton.StartHost();
+        yield return null;
+    }
+
+    IEnumerator ConfigureTransportAndStartNgoAsConnectingPlayer() // Join Lobby
+    {
+        // Populate RelayJoinCode beforehand through the UI
+        var clientRelayUtilityTask = JoinRelayServerFromJoinCode(tryJoinCode);
+
+        while (!clientRelayUtilityTask.IsCompleted)
+        {
+            yield return null;
+        }
+
+        if (clientRelayUtilityTask.IsFaulted)
+        {
+            Debug.Log("RLManager: Exception thrown when attempting to connect to Relay Server. Exception: " + clientRelayUtilityTask.Exception.Message);
+            yield break;
+        }
+
+        var (ipv4address, port, allocationIdBytes, connectionData, hostConnectionData, key) = clientRelayUtilityTask.Result;
+
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetClientRelayData(ipv4address, port, allocationIdBytes, key, connectionData, hostConnectionData, true);
+        NetworkManager.Singleton.StartClient();
+        yield return null;
+    }
+
+    public async Task<(string ipv4address, ushort port, byte[] allocationIdBytes, byte[] connectionData, byte[] key, string joinCode)> AllocateRelayServerAndGetJoinCode(int maxConnections, string region = null)
+    {
+        Allocation allocation;
+        string createJoinCode;
+        try
+        {
+            allocation = await RelayService.Instance.CreateAllocationAsync(maxConnections, region);
+        }
+        catch (Exception e)
+        {
+            Debug.Log($"RLManager:Relay create allocation request failed {e.Message}");
+            throw;
+        }
+
+        Debug.Log($"server: {allocation.ConnectionData[0]} {allocation.ConnectionData[1]}");
+        Debug.Log($"server: {allocation.AllocationId}");
+
+        try
+        {
+            createJoinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+        }
+        catch
+        {
+            Debug.Log("RLManager: Relay create join code request failed");
+            throw;
+        }
+
+        var dtlsEndpoint = allocation.ServerEndpoints.First(e => e.ConnectionType == "dtls");
+        return (dtlsEndpoint.Host, (ushort)dtlsEndpoint.Port, allocation.AllocationIdBytes, allocation.ConnectionData, allocation.Key, createJoinCode);
+    }
+
+    public async Task<(string ipv4address, ushort port, byte[] allocationIdBytes, byte[] connectionData, byte[] hostConnectionData, byte[] key)> JoinRelayServerFromJoinCode(string joinCode)
+    {
+        JoinAllocation allocation;
+        try
+        {
+            allocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+        }
+        catch
+        {
+            Debug.Log("RLManager: Relay create join code request failed");
+            throw;
+        }
+
+        Debug.Log($"client: {allocation.ConnectionData[0]} {allocation.ConnectionData[1]}");
+        Debug.Log($"host: {allocation.HostConnectionData[0]} {allocation.HostConnectionData[1]}");
+        Debug.Log($"client: {allocation.AllocationId}");
+
+        var dtlsEndpoint = allocation.ServerEndpoints.First(e => e.ConnectionType == "dtls");
+        return (dtlsEndpoint.Host, (ushort)dtlsEndpoint.Port, allocation.AllocationIdBytes, allocation.ConnectionData, allocation.HostConnectionData, allocation.Key);
+    }
+}
