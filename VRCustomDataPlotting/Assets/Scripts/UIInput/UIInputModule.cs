10de03dc352b4c29df779f6955bfb87ae2266725
Lucia Hughes <lucia.hughes@mymail.champlain.edu>
2022-07-11 11:40:59 -0400

    Just making sure everything is up to date

--- VRCustomDataPlotting/Assets/Scripts/UIInput/UIInputModule.cs
+++ VRCustomDataPlotting/Assets/Scripts/UIInput/UIInputModule.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 
-//public class UIInputModule : PointerInputModule
-//{
-//
-//    public override void Process()
-//    {
-//        if (GetPointerData(id, out EventSystems.PointerEventData data);
-//        EventSystem.current.RaycastAll(m_PointerData, m_RaycastResultCache);
-//    }
-//}
+public class UIInputModule : PointerInputModule
+{
+
+    public override void Process()
+    {
+        EventSystem.current.RaycastAll(data, m_RaycastResultCache);
+
+        foreach (RaycastResult raycastResult in m_RaycastResultCache)
+        {
+                Debug.Log(raycastResult);
+                IPointerClickHandler clickHandler = raycastResult.gameObject.GetComponent<IPointerClickHandler>();
+                if (clickHandler == null)
+                    clickHandler = raycastResult.gameObject.GetComponentInParent<IPointerClickHandler>();
+
+                if (clickHandler != null)
+                {
+                    PointerEventData pointerEventData = new PointerEventData(EventSystem.current);
+                    clickHandler.OnPointerClick(pointerEventData);
+                }
+        }
+        
+    }
+}
