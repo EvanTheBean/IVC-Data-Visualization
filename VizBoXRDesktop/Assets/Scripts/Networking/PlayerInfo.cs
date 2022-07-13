547888aa17131f9a7b66a27f0e9ebd956e222177
Lucia Hughes <luxiextra@gmail.com>
2022-07-13 14:29:24 -0400

    Cardboard VR ready for graphs. Also adds UI stuff
    * Updates android build settings for VR setup
    * Imports new packages for VR
    * Preparations for UI display of what each player is doing

--- VizBoXRDesktop/Assets/Scripts/Networking/PlayerInfo.cs
+++ VizBoXRDesktop/Assets/Scripts/Networking/PlayerInfo.cs
@@ -1,10 +1,8 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.Collections;
 using Unity.Netcode;
+using UnityEngine.Events;
 using System;
-
 // DESKTOP
 
 public enum Activity
@@ -14,19 +12,21 @@ public enum Activity
     VR
 }
 
+[Serializable]
+public class PlayerEvent : UnityEvent<string, Activity> { };
+
 public class PlayerInfo : NetworkBehaviour
 {
     Dictionary<ulong, (string, Activity)> players = new Dictionary<ulong, (string, Activity)>();
     NetworkVariable<int> playerCount = new NetworkVariable<int>(0);
 
+    public PlayerEvent OnPlayerJoin;
+    public PlayerEvent OnPlayerLeave;
+    public PlayerEvent OnChangeActivity;
+
     private void Start()
     {
-        playerCount.OnValueChanged += OnPlayerJoin;
         NetworkManager.OnClientDisconnectCallback += LeaveLobbyServerRpc;
-    }
-
-    private void OnPlayerJoin(int previousValue, int newValue)
-    {
     }
 
     [ServerRpc(RequireOwnership = false)]
@@ -35,17 +35,17 @@ public class PlayerInfo : NetworkBehaviour
         players.Add(id, (username, Activity.Annotating));
         Debug.Log(username + " joined the lobby.");
         playerCount.Value++;
-
-        //TODO UI
+        OnPlayerJoin.Invoke(username, Activity.Annotating);
     }
 
     [ServerRpc(RequireOwnership = false)]
     void LeaveLobbyServerRpc(ulong id)
     {
         Debug.Log(players[id].Item1 + " left the lobby.");
+        OnPlayerLeave.Invoke(players[id].Item1, Activity.Annotating);
         players.Remove(id);
         playerCount.Value--;
-        //TODO UI
+        
     }
 
     [ServerRpc(RequireOwnership = false)]
@@ -54,6 +54,7 @@ public class PlayerInfo : NetworkBehaviour
         (string, Activity) playerData = players[id];
         playerData.Item2 = activity;
         players[id] = playerData;
+        OnChangeActivity?.Invoke(playerData.Item1, activity);
         Debug.Log(playerData.Item1 + " switched to activity " + activity.ToString());
     }
 
